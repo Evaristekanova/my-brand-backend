d981921be8a688de673c23261fb17cae6b94c90c
Jean Iyakaremye <jean.iyakaremye@rura.rw>
2023-01-10 06:50:17 +0200

    developing branch

---
+++ app.js
@@ -0,0 +1,9 @@
+const { log } = require('console')
+const express = require('express')
+const {MongoClient} = require('mongodb')
+const app = express()
+app.get('/' , (req, res)=>{
+    res.status(200).send("hello from server side")
+})
+module.exports = app
+
---
+++ server.js
@@ -0,0 +1,4 @@
+const app = require('./app')
+app.listen(8000, ()=>{
+    console.log('server starting');
+})

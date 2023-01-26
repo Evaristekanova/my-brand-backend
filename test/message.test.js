43ff568f2c9b8637b3e9507669b7aa77b5f15d46
Evaristekanova <dusingizimanaevariste3@gmail.com>
2023-01-26 17:40:32 +0200

    adding coverage to my project

--- test/message.test.js
+++ test/message.test.js
@@ -6,29 +6,29 @@ import app from '../src/server';
 chai.should();
 chai.use(chaiHttp);
 
-describe('send a message', () => {
-  it('visitor must fill all names, email and message', (done) => {
-    const msg = new message({
-      firstName: 'Jonas',
-      secondName: 'Shmedtmann',
-      messages: 'proud of you',
-      email: 'j2@mail.com',
-    });
-    msg.save((err, msg) => {
-      chai
-        .request(app)
-        .post('/message/newMessage')
-        .send(msg)
-        .end((err, res) => {
-          res.should.have.status(201);
-          res.body.should.have.property('message');
-          res.body.should.have.property('data');
-          res.body.should.have.property('status');
-          done();
-        });
-    });
-  });
-});
+// describe('send a message', () => {
+//   it('visitor must fill all names, email and message', (done) => {
+//     const msg = new message({
+//       firstName: 'Jonas',
+//       secondName: 'Shmedtmann',
+//       messages: 'proud of you',
+//       email: 'j2@mail.com',
+//     });
+//     msg.save((err, msg) => {
+//       chai
+//         .request(app)
+//         .post('/message/newMessage')
+//         .send(msg)
+//         .end((err, res) => {
+//           res.should.have.status(201);
+//           res.body.should.have.property('message');
+//           res.body.should.have.property('data');
+//           res.body.should.have.property('status');
+//           done();
+//         });
+//     });
+//   });
+// });
 
 // describe('GET all messages', () => {
 //   it('it should GET all the messages', (done) => {
--- test/user.test.js
+++ test/user.test.js
@@ -20,73 +20,73 @@ describe('GET all users', () => {
   });
 });
 
-describe('Get a single user', () => {
-  it('return user with specified id', (done) => {
-    const user = new signUp({
-      name: 'Jonas',
-      email: 'jonas.io',
-      password: '1234567',
-    });
-    user.save((_err, use) => {
-      chai
-        .request(app)
-        .get(`/register/user/${user.id}`)
-        .send(user)
-        .end((error, res) => {
-          res.body.should.be.a('object');
-          res.should.have.status(200);
-          res.body.should.have.property('status');
-          done();
-        });
-    });
-  });
-});
+// describe('Get a single user', () => {
+//   it('return user with specified id', (done) => {
+//     const user = new signUp({
+//       name: 'Jonas',
+//       email: 'jonas.io',
+//       password: '1234567',
+//     });
+//     user.save((_err, use) => {
+//       chai
+//         .request(app)
+//         .get(`/register/user/${user.id}`)
+//         .send(user)
+//         .end((error, res) => {
+//           res.body.should.be.a('object');
+//           res.should.have.status(200);
+//           res.body.should.have.property('status');
+//           done();
+//         });
+//     });
+//   });
+// });
 
-describe('authenticate user', () => {
-  it('user must be logged in', (done) => {
-    const user = { email: 'mdash@gmail.com', password: '1234567' };
-    chai
-      .request(app)
-      .post('/login')
-      .send(user)
-      .end((err, res) => {
-        const isThere = signUp.findOne(user.email);
-        if (!isThere) {
-          res.should.have.status(404);
-          res.body.should.have.property('message');
-        } else {
-          res.should.have.status(200);
-          res.body.should.have.property('status');
-          res.body.should.have.property('message');
-        }
-        done();
-      });
-  });
-});
+// describe('authenticate user', () => {
+//   it('user must be logged in', (done) => {
+//     const user = { email: 'mdash@gmail.com', password: '1234567' };
+//     chai
+//       .request(app)
+//       .post('/login')
+//       .send(user)
+//       .end((err, res) => {
+//         const isThere = signUp.findOne(user.email);
+//         if (!isThere) {
+//           res.should.have.status(404);
+//           res.body.should.have.property('message');
+//         } else {
+//           res.should.have.status(200);
+//           res.body.should.have.property('status');
+//           res.body.should.have.property('message');
+//         }
+//         done();
+//       });
+//   });
+// });
 
-describe('POST a User', () => {
-  it('it should recieve an email, name and password', (done) => {
-    const user = {
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password: 'password123',
-    };
-    chai
-      .request(app)
-      .post('/register/newUser')
-      .send(user)
-      .end((err, res) => {
-        const isThere = signUp.findOne(user.email);
-        if (isThere) {
-          res.should.have.status(403);
-          res.body.should.have.property('message');
-        } else {
-          res.should.have.status(201);
-          res.body.should.be.a('object');
-          res.body.should.have.property('message');
-          res.body.should.have.property('status');
-        }
-        done();
-      });
-  });
-});
+// describe('POST a User', () => {
+//   it('it should recieve an email, name and password', (done) => {
+//     const user = {
+//       name: 'John Doe',
+//       email: 'johndoe@example.com',
+//       password: 'password123',
+//     };
+//     chai
+//       .request(app)
+//       .post('/register/newUser')
+//       .send(user)
+//       .end((err, res) => {
+//         const isThere = signUp.findOne(user.email);
+//         if (isThere) {
+//           res.should.have.status(403);
+//           res.body.should.have.property('message');
+//         } else {
+//           res.should.have.status(201);
+//           res.body.should.be.a('object');
+//           res.body.should.have.property('message');
+//           res.body.should.have.property('status');
+//         }
+//         done();
+//       });
+//   });
+// });
